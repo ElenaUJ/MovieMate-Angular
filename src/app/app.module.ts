249eb1182e36f752ea21a34d85d6fa7f3dd77515
ElenaUJ <elena-ha@web.de>
2023-07-06 12:00:49 -0400

    Set up and configured HttpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
+// Module houses important dependencies so they can be used across components
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+// Simplified API facilitation cimmunication of client app with API or server-side
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
