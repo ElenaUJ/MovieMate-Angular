a2a4496369d73521eff6dbdbf785a4bba65367fc
ElenaUJ <elena-ha@web.de>
2023-07-13 15:08:24 -0400

    Added success message to registration handling

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -43,8 +43,7 @@ export class UserRegistrationFormComponent implements OnInit {
         // Will close modal on success
         this.dialogRef.close();
         // Second argument 'OK' represents action button
-        // Question: I noticed that the message shown in the snackBar in case of success is [Object object], obviously not ideal. Shall I write a custom message that the user successfully signed up?
-        this.snackBar.open(result, 'OK', {
+        this.snackBar.open('Successfully signed up. Please log in!', 'OK', {
           duration: 2000,
         });
       },
