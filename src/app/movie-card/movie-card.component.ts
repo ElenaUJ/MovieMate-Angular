832fe40da299d0ae47ac7a73a0480804aff371f9
ElenaUJ <elena-ha@web.de>
2023-07-18 13:36:48 -0400

    Added MovieCard component

---
+++ src/app/movie-card/movie-card.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+@Component({
+  selector: 'app-movie-card',
+  templateUrl: './movie-card.component.html',
+  styleUrls: ['./movie-card.component.scss'],
+})
+// Question: Don't I have to add implements OnInit here, too, just like in the Login and Register components? In the course script that's missing
+export class MovieCardComponent {
+  movies: any[] = [];
+
+  constructor(public fetchApiData: FetchApiDataService) {}
+
+  ngOnInit(): void {
+    this.getMovies();
+  }
+
+  // Question: Why is the return type void? That doesn't make sense. I guess I should define the movies array Observable type, or at least write Observable<any>, no?
+  getMovies(): void {
+    this.fetchApiData.getAllMovies().subscribe((resp: any) => {
+      this.movies = resp;
+      console.log(this.movies);
+      return this.movies;
+    });
+  }
+}
